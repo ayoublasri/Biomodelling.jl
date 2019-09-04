404cbc6fc985bf674950aa7c77dbf4a6d118bdda
Ayoub Lasri <ayoublasri@RCSIADMINs-MacBook-Pro.local>
2019-09-04 14:21:52 +0100

    adding test case

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,3 +18,24 @@ function test_1()
 end
 
 @test test_1() == 4
+
+function test_2()
+
+    k1 = 4e-5
+    k2 = 50
+    k3 = 10
+    k4 = 25
+
+    Reaction1 = (name = "trans", rate = k1, reactants = [:A ; :B], products =[:A] , coeff_rea = [2;1] , coeff_pro = [3] )
+    Reaction2 = (name = "birth", rate = k2, reactants = [:NULL], products =[:A] , coeff_rea = [1] , coeff_pro = [1] )
+    Reaction3 = (name = "death", rate = k3, reactants = [:A], products =[:NULL] , coeff_rea = [1] , coeff_pro = [1] )
+    Reaction4 = (name = "transf", rate = k4, reactants = [:NULL], products =[:B] , coeff_rea = [1] , coeff_pro = [1] )
+    model = (Reaction1, Reaction2, Reaction3, Reaction4)
+
+    initiale_population = [:NULL 0;:A 10;:B 10]
+
+    data = Biomodelling.Donne(model,initiale_population,1000,1)
+    return data.stoichio
+end
+
+@test test_2() == [0 -1 1 0; 0 0 1 0; -1 0 0 0; 0 0 1 0]
