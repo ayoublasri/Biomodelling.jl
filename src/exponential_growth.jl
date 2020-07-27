ad1d9173fa6d939e57791e5e092c173c42328b7f
Ayoub Lasri <ayoublasri@RCSIADMINs-MacBook-Pro.local>
2020-07-27 15:12:37 +0100

    update

--- src/exponential_growth.jl
+++ src/exponential_growth.jl
@@ -47,7 +47,7 @@ function division(V::Array{Float64,1},V_f::Float64,expression,div_noise::Float64
         V_D .= temp_V[out] .- V[out]
         expression_D = zeros(sum(out.==true),size(expression,2))
         expression_D = expression[out,:]
-        expression_D = temp_expression[out,bb] .- expression[out,bb]
+        expression_D[:,bb] = temp_expression[out,bb] .- expression[out,bb]
     end
     return V, V_D, expression, expression_D
 end
