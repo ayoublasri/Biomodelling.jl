eb7109b927f61c8810e024dd42e9d697a444bdb8
Ayoub Lasri <ayoublasri@RCSIADMINs-MacBook-Pro.local>
2020-07-27 14:38:19 +0100

    update cell division

--- src/exponential_growth.jl
+++ src/exponential_growth.jl
@@ -45,8 +45,8 @@ function division(V::Array{Float64,1},V_f::Float64,expression,div_noise::Float64
         end
         V_D = zeros(sum(out.==true))
         V_D .= temp_V[out] .- V[out]
-        expression_D = expression[out,:]
-        expression_D[out,bb] = temp_expression[out,bb] .- expression[out,bb]
+        expression_D = zeros(sum(out.==true),bb)
+        expression_D = temp_expression[out,bb] .- expression[out,bb]
     end
     return V, V_D, expression, expression_D
 end
